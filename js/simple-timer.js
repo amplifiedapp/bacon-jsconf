68770d6f1c09a629c9944d32504119bad74eea28
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2014-03-09 20:42:42 -0300

    Rearrange timer example to be full bacon

--- js/simple-timer.js
+++ js/simple-timer.js
@@ -1,48 +1,55 @@
-var minutesGen = clockNumbersGenerator(clockLongNumbers);
-var hoursGen = clockNumbersGenerator(clockShortNumbers);
-
-var seconds = Bacon.repeatedly(1000, clockLongNumbers)
-  .map(normalizeNumber);
-
-seconds.assign($("#seconds"), 'text');
+// Set up timer seconds, minutes, and hours
+var seconds = Bacon.interval(1000, 'tick')
+  .scan(0, function(minutes) {
+    return minutes == 59 ? 0 : (minutes + 1);
+  })
+  .map(toTwoDigitsString);
 
 var minutes = seconds
-  .filter(function(value) {
-    return value == "00";
+  .changes()
+  .filter(isOClock)
+  .scan(0, function(minutes) {
+    return minutes == 59 ? 0 : (minutes + 1);
   })
-  .map(function() { return minutesGen.next(); });
-
-minutes.assign($("#minutes"), 'text');
+  .map(toTwoDigitsString);
 
 var hours = minutes
-  .filter(function(value) {
-    return value == "00";
+  .changes()
+  .filter(isOClock)
+  .scan(0, function(hours) {
+    return hours == 23 ? 0 : (hours + 1);
   })
-  .map(function() { return hoursGen.next(); });
+  .map(toTwoDigitsString);
 
-hours.assign($("#hour"), 'text');
-
-var fullHour = hours.toProperty('00').map(Array)
-  .combine(minutes.toProperty('00').map(Array), '.concat')
-  .combine(seconds.toProperty('00').map(Array), '.concat')
+// Build full hour from the 3 properties
+var fullHour = hours.map(Array)
+  .combine(minutes.map(Array), '.concat')
+  .combine(seconds.map(Array), '.concat')
   .map(function(arr) {
     return arr.join(':');
   });
 
+// Set up alarms
 var alarmTime = $('#alarmTime').asEventStream('keypress');
 var alarmSet = $('#setAlarm').asEventStream('click');
 
 var currentAlarm = alarmTime.sampledBy(alarmSet)
   .map('.target').map('.value');
 
-currentAlarm.assign(function(alarm) {
-  $('#currentAlarm').append('<li>'+alarm+'</li>');
-});
-
 var alarmSignal = currentAlarm.flatMap(function(time) {
   return fullHour.filter(function(current) { return current == time; });
 });
 
+// Bind everything to the DOM
+seconds.assign($("#seconds"), 'text');
+minutes.assign($("#minutes"), 'text');
+hours.assign($("#hour"), 'text');
+
+currentAlarm.assign(function(alarm) {
+  $('#currentAlarm').append('<li>'+alarm+'</li>');
+});
+
+// Fire the alarm!!
 alarmSignal.onValue(function(time) {
   alert('Alarm sounds! It\'s ' + time);
 });
--- js/util.js
+++ js/util.js
@@ -1,21 +1,10 @@
-var range = function(i) { return i?range(i-1).concat(i):[] };
-var clockLongNumbers = range(59).concat(0);
-var clockShortNumbers = range(23).concat(0);
+var range = function(i) { return i ? range(i-1).concat(i) : []; };
+var secondsNumbers = range(59).concat(0);
 
-var normalizeNumber = function (val) {
-  if (val < 10) {
-    return "0" + val;
-  } else {
-    return ''+val;
-  }
+var toTwoDigitsString = function (val) {
+  return (val < 10 ? "0" : '') + val;
 };
 
-var clockNumbersGenerator = function (range) {
-  var validClockNumbers = range;
-  var i = 0;
-  while (true) {
-    if ( i == validClockNumbers.length) { i = 0; }
-    yield normalizeNumber(validClockNumbers[i]);
-    i++;
-  }
-};
+function isOClock(time) {
+  return time == '00';
+}
