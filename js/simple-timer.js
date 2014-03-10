ef27726c3dd1a10190be6c3a556f630cb3240286
Sergio Rafael Gianazza <sgianazza@gmail.com>
2014-03-10 00:34:38 -0300

    Fix variable name (again)

--- js/simple-timer.js
+++ js/simple-timer.js
@@ -1,15 +1,15 @@
 // Set up timer seconds, minutes, and hours
 var seconds = Bacon.interval(1000, 'tick')
-  .scan(0, function(minutes) {
-    return minutes == 59 ? 0 : (minutes + 1);
+  .scan(0, function(seconds) {
+    return seconds == 59 ? 0 : (seconds + 1);
   })
   .map(toTwoDigitsString);
 
 var minutes = seconds
   .changes()
   .filter(isOClock)
-  .scan(0, function(seconds) {
-    return seconds == 59 ? 0 : (seconds + 1);
+  .scan(0, function(minutes) {
+    return minutess == 59 ? 0 : (minutess + 1);
   })
   .map(toTwoDigitsString);
 
