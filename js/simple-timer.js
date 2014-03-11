d057d461da3aa03d235425bab5d40365e2bf4712
Sergio Rafael Gianazza <sgianazza@gmail.com>
2014-03-11 11:46:34 -0300

    live coding for simple-timer

--- js/simple-timer.js
+++ js/simple-timer.js
@@ -1,55 +1,25 @@
 // Set up timer seconds, minutes, and hours
-var seconds = Bacon.interval(1000, 'tick')
-  .scan(0, function(seconds) {
-    return seconds == 59 ? 0 : (seconds + 1);
-  })
-  .map(toTwoDigitsString);
+var seconds = null;
 
-var minutes = seconds
-  .changes()
-  .filter(isOClock)
-  .scan(0, function(minutes) {
-    return minutes == 59 ? 0 : (minutes + 1);
-  })
-  .map(toTwoDigitsString);
+var minutes = null;
 
-var hours = minutes
-  .changes()
-  .filter(isOClock)
-  .scan(0, function(hours) {
-    return hours == 23 ? 0 : (hours + 1);
-  })
-  .map(toTwoDigitsString);
+var hours = null;
 
 // Build full hour from the 3 streams
-var fullHour = hours.map(Array)
-  .combine(minutes.map(Array), '.concat')
-  .combine(seconds.map(Array), '.concat')
-  .map(function(arr) {
-    return arr.join(':');
-  });
+var fullHour = null;
 
 // Set up alarms
 var alarmTime = $('#alarmTime').asEventStream('keypress');
 var alarmSet = $('#setAlarm').asEventStream('click');
 
-var currentAlarm = alarmTime.sampledBy(alarmSet)
-  .map('.target').map('.value');
+var currentAlarm = null;
 
-var alarmSignal = currentAlarm.flatMap(function(time) {
-  return fullHour.filter(function(current) { return current == time; });
-});
+var alarmSignal = null;
 
 // Bind everything to the DOM
-seconds.assign($("#seconds"), 'text');
-minutes.assign($("#minutes"), 'text');
-hours.assign($("#hour"), 'text');
-
-currentAlarm.assign(function(alarm) {
-  $('#currentAlarm').append('<li>'+alarm+'</li>');
-});
+// #hour, #minutes, #seconds
+// Alarms list => #currentAlarm
 
 // Fire the alarm!!
-alarmSignal.onValue(function(time) {
-  alert('Alarm sounds! It\'s ' + time);
-});
+
+
--- js/util.js
+++ js/util.js
@@ -5,6 +5,15 @@ var toTwoDigitsString = function (val) {
   return (val < 10 ? "0" : '') + val;
 };
 
-function isOClock(time) {
+var isOClock = function (time) {
   return time == '00';
+}
+
+var nextSecond = function(second) {
+  return second == 59 ? 0 : (second + 1);
+}
+
+var nextMinute = nextSecond;
+var nextHour = function(hour) {
+  return hour == 23 ? 0 : (hour + 1);
 }
