8d4f97f190991a324dca5303790f6ddbe9df4e85
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2014-03-11 15:48:35 -0300

    Update backbone example file

--- js/backbone.js
+++ js/backbone.js
@@ -1,31 +1,22 @@
 // Get an event stream for all add remove and reset events in the collection
-var reposProp = eventStream(repos, 'add remove reset').map(function(args) {
-    return args[1];
+var reposProp = null;
 
 // and make it a property!
-}).toProperty(repos);
 
-// Results count is easy!
-var resultsCount = reposProp.map('.models').map('.length');
+// Results count
+var resultsCount = null;
 
 // Render the repos
-reposProp.map(renderRepos).assign($('#list'), 'html');
+
 // Render the counts
-resultsCount.assign($('#resultsCount'), 'text');
+
+
+// Just for fun, we assign the input to a backbone property
+var filterSearches = null;
+
 
 // Create a model for the filter input
 var filter = new Backbone.Model({ filter: '' });
 
-// Just for fun, we assign the input to a backbone property
-var filterSearches = $('#filter').asEventStream('input')
-    .debounce(300)
-    .map('.target').map('.value')
-    .assign(filter, 'set', 'filter');
-
-// We make it a Bacon Property!
-modelProperty(filter, 'filter')
-    .skipDuplicates()
-    .filter(function(text) {
-        return text.length >= 3 || text === '';
-    })
-    .onValue(function(text) { repos.search(text); });
+// We make it a Bacon Property
+// We bind it to the DOM
