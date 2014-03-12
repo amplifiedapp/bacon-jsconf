94a18eb1d4350f7a3b1d35b3f3a6b6b4be6fe5c2
Sergio Rafael Gianazza <sgianazza@gmail.com>
2014-03-12 09:25:42 -0300

    Removed unused function

--- js/util.js
+++ js/util.js
@@ -1,6 +1,3 @@
-var range = function(i) { return i ? range(i-1).concat(i) : []; };
-var secondsNumbers = range(59).concat(0);
-
 var toTwoDigitsString = function (val) {
   return (val < 10 ? "0" : '') + val;
 };
